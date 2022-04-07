d9ce01713782080f16f56369943256519850f4e0
Caius Durling <dev@caius.name>
2022-04-07 17:16:21 +0100

    Attempt to safely not promote deploy

---
+++ .semaphore/scripts/promote_smartly.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+set -o xtrace
+set -o errexit
+set -o nounset
+set -o pipefail
+set -o noclobber
+
+promote_smartly="false"
+
+if [[ "${promote_smartly}" = "true"]]; then
+  curl --fail -H @/tmp/sem-api-header-caius --data \"name=Deploy+deploy+deploy&pipeline_id=${SEMAPHORE_PIPELINE_ID}\" ${SEMAPHORE_ORGANIZATION_URL}/api/v1/promotions
+else
+  echo "Not promoting deploy"
+fi
