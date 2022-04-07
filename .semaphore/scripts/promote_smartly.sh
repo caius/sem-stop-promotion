c35e1e80e4d5ef5cb5e1358ab54636ee13398e97
Caius Durling <dev@caius.name>
2022-04-07 17:22:14 +0100

    Deploy

--- .semaphore/scripts/promote_smartly.sh
+++ .semaphore/scripts/promote_smartly.sh
@@ -6,7 +6,7 @@ set -o nounset
 set -o pipefail
 set -o noclobber
 
-promote_smartly="false"
+promote_smartly="true"
 
 if [[ "${promote_smartly}" = "true" ]]; then
   curl --fail -H @/tmp/sem-api-header-caius --data "name=Deploy+deploy+deploy&pipeline_id=${SEMAPHORE_PIPELINE_ID}" "${SEMAPHORE_ORGANIZATION_URL}/api/v1/promotions"
